f4f5b653bf7930852616d4ff82b0033d7652f44b
Atu77l <kesharwaniatul9935@gmail.com>
2022-10-30 23:18:54 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose=require('mongoose');
 
 const connectDB=async ()=>{
     try{
-        // console.log(process.env.MONGO_URL);
         const conn=await mongoose.connect(process.env.MONGO_URL,{
             useNewUrlParser:true,
             useUnifiedTopology:true
